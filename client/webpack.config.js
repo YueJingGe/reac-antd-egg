fab9a2fa481f608d5a31c57dce919d2a8e173997
jingge yue <jinggeyue@jinggedeMacBook-Pro.local>
2019-09-18 17:11:14 +0800

    更新数据库

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -77,7 +77,7 @@ module.exports = {
     open: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:7002",
+        target: "http://127.0.0.1:7001",
         changeOrigin: true
       }
     },
